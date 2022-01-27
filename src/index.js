0f49866cc19523cf445ae75dff83ad4a74d36731
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-27 10:15:53 +0100

    remove autocomplete on Message and added username for client side

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
         callback()
     })
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left!`))
         }
     })
 })
